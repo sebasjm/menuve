a83642869b7a5eee25c6b8283e3907601de1c55b
Sebastian Javier Marchano <sebasjm@gmail.com>
2012-09-01 17:24:36 -0300

    resto reference

--- site/web/src/main/java/com/menuve/model/Menu.java
+++ site/web/src/main/java/com/menuve/model/Menu.java
@@ -8,58 +8,45 @@ import java.util.Date;
 public class Menu {
 
     private Long id;
-    private Long idResto;
+    private Resto resto;
     private String description;
     private String name;
     private Date createdOn;
 
     public Long getId() {
-
         return id;
     }
 
     public void setId(Long id) {
-
         this.id = id;
     }
 
-    public Long getIdResto() {
-
-        return idResto;
-    }
-
-    public void setIdResto(Long idResto) {
-
-        this.idResto = idResto;
-    }
-
     public String getDescription() {
-
         return description;
     }
 
     public void setDescription(String description) {
-
         this.description = description;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public Date getCreatedOn() {
-
         return createdOn;
     }
 
     public void setCreatedOn(Date createdOn) {
-
         this.createdOn = createdOn;
     }
+
+    public Resto getResto() {
+        return resto;
+    }
+    
 }
--- site/web/src/main/java/com/menuve/model/Resto.java
+++ site/web/src/main/java/com/menuve/model/Resto.java
@@ -14,52 +14,42 @@ public class Resto {
     private Date loggedOn;
 
     public Long getId() {
-
         return id;
     }
 
     public void setId(Long id) {
-
         this.id = id;
     }
 
     public String getName() {
-
         return name;
     }
 
     public void setName(String name) {
-
         this.name = name;
     }
 
     public String getAddress() {
-
         return address;
     }
 
     public void setAddress(String address) {
-
         this.address = address;
     }
 
     public Date getCreatedOn() {
-
         return createdOn;
     }
 
     public void setCreatedOn(Date createdOn) {
-
         this.createdOn = createdOn;
     }
 
     public Date getLoggedOn() {
-
         return loggedOn;
     }
 
     public void setLoggedOn(Date loggedOn) {
-
         this.loggedOn = loggedOn;
     }
 }
