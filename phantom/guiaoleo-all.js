c362763fc39d396d05b6196ed9a0e4e8b588602f
Sebastian Javier Marchano <sebasjm@gmail.com>
2012-10-16 11:05:57 -0300

    ident

--- phantom/guiaoleo-all.js
+++ phantom/guiaoleo-all.js
@@ -12,10 +12,10 @@ page.open("http://www.guiaoleo.com.ar/search/list/todos?page="+system.args[1], f
     if ( status === "success" ) {
         page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
             page.evaluate(function() {
-		var pepe = new Array; 
-		$.each($(".restoDatosNombre").children('a'), function(i,a){ 
-			console.log("resto -> " + a.href);
-		});
+                var pepe = new Array; 
+                $.each($(".restoDatosNombre").children('a'), function(i,a){ 
+                    console.log("resto -> " + a.href);
+                });
             });
             phantom.exit();
         });
--- phantom/guiaoleo-resto.js
+++ phantom/guiaoleo-resto.js
@@ -1,7 +1,8 @@
 // Read the Phantom webpage '#intro' element text using jQuery and "includeJs"
 var system = require('system');
+var client = require('webpage').create();
 
-var page = require('webpage').create();
+client.injectJs('lib/jquery.min.js');
 
 /*
 page.onConsoleMessage = function(msg) {
@@ -11,83 +12,90 @@ page.onConsoleMessage = function(msg) {
 var pages = new Array;
 var resto = new Object;
 
-pages.push({url:system.args[1],extractor: get_resto_data});
+pages.push({
+    url:system.args[1],
+    extractor: get_resto_data
+});
 next();
 
 
 function get_resto_data() {
-                var restoDatos = $(".restoDatos");
-                resto.nombre = restoDatos.find(".restoDatosNombre").text().replace(/\s+/g, ' ');
-                resto.direccion = restoDatos.find(".restoDatosDireccion").text().replace(/\s+/g, ' ');
-                resto.lugar = restoDatos.find(".restoDatosDireccion > a").attr("href");
-                resto.cocina = new Array;
-                restoDatos.find(".restoDatosCocina > a").each(function(i,e){
-                        resto.cocina.push( $(e).attr("href") );
-                });
-                resto.contacto = restoDatos.find(".restoDatosTelefono").contents()[0].data.split(' ').join('').trim();
-                resto.contactos = new Array;
-                resto.site = new Array;
-                restoDatos.find(".restoDatosTelefono > a[href] ").each(function(i,e){
-                        var href = $(this).attr('href');
-                        resto.contactos.push(href)
-                        if ( !!href.match('restaurante/urlredirect/nombreClave') ) { 
-			    resto.site.push(href);
-			    pages.push({url:'http://www.guiaoleo.com.ar/'+href, extractor: get_resto_homepage});
-			}
-                });
-		resto.horarios = new Array;
-		restoDatos.find(".restoDatosHorarios").each(function(i,e){ 
-			resto.horarios.push( $(this).text().replace(/\s+/g, ' ').replace(/horario:/i,''));
-		});
+    var restoDatos = $(".restoDatos");
+    resto.nombre = restoDatos.find(".restoDatosNombre").text().replace(/\s+/g, ' ');
+    resto.direccion = restoDatos.find(".restoDatosDireccion").text().replace(/\s+/g, ' ');
+    resto.lugar = restoDatos.find(".restoDatosDireccion > a").attr("href");
+    resto.cocina = new Array;
+    restoDatos.find(".restoDatosCocina > a").each(function(i,e){
+        resto.cocina.push( $(e).attr("href") );
+    });
+    resto.contacto = restoDatos.find(".restoDatosTelefono").contents()[0].data.split(' ').join('').trim();
+    resto.contactos = new Array;
+    resto.site = new Array;
+    restoDatos.find(".restoDatosTelefono > a[href] ").each(function(i,e){
+        var href = $(this).attr('href');
+        resto.contactos.push(href)
+        if ( !!href.match('restaurante/urlredirect/nombreClave') ) { 
+            resto.site.push(href);
+            pages.push({
+                url:'http://www.guiaoleo.com.ar/'+href, 
+                extractor: get_resto_homepage
+            });
+        }
+    });
+    resto.horarios = new Array;
+    restoDatos.find(".restoDatosHorarios").each(function(i,e){ 
+        resto.horarios.push( $(this).text().replace(/\s+/g, ' ').replace(/horario:/i,''));
+    });
 
-		resto.mediosPago = new Array;
-		restoDatos.find(".restoDatosMediosPago").find("img").each(function(i,e){
-			resto.mediosPago.push( $(e).attr("title") );
-		});
-		resto.puntos = new Array;
-		$(".calificaciones").find("ul.restoDatosRanking").find(".rankBar").parent().each(function(i,e){
-			var punto = new Object;
-			punto.nombre = $(e).find("label").text();
-			punto.valor = $(e).find(".rank")[0].style.width.replace(/pt/,''); 
-			resto.puntos.push(punto);
-		});
-		resto.calificaciones = new Array;
-		$("#boxRecomendarEtiquetas").find(".rankBar").parent().each(function(i,e){
-                        var punto = new Object;
-                        punto.nombre = $(e).find("label").text();
-                        punto.valor = $(e).find(".rank").text().replace(/ usuarios/g,'');
-                        resto.calificaciones.push(punto);
-		});
-		resto.desc = new Array;
-		$(".restoEspecificaciones > p").each(function(i,e){
-			resto.desc.push( $(e).text().replace(/\s\s+/g,'') );
-		});
-		resto.precio = $(".colSide > .restoPrecios").text().replace(/\s\s+/g,'');
-		resto.caracteristicas = new Array;
-		$(".goLink").find("li:not(.disabled)").each(function(i,e){
-			resto.caracteristicas.push( $(e).text().replace(/\s\s+/g,'') );
-		});
+    resto.mediosPago = new Array;
+    restoDatos.find(".restoDatosMediosPago").find("img").each(function(i,e){
+        resto.mediosPago.push( $(e).attr("title") );
+    });
+    resto.puntos = new Array;
+    $(".calificaciones").find("ul.restoDatosRanking").find(".rankBar").parent().each(function(i,e){
+        var punto = new Object;
+        punto.nombre = $(e).find("label").text();
+        punto.valor = $(e).find(".rank")[0].style.width.replace(/pt/,''); 
+        resto.puntos.push(punto);
+    });
+    resto.calificaciones = new Array;
+    $("#boxRecomendarEtiquetas").find(".rankBar").parent().each(function(i,e){
+        var punto = new Object;
+        punto.nombre = $(e).find("label").text();
+        punto.valor = $(e).find(".rank").text().replace(/ usuarios/g,'');
+        resto.calificaciones.push(punto);
+    });
+    resto.desc = new Array;
+    $(".restoEspecificaciones > p").each(function(i,e){
+        resto.desc.push( $(e).text().replace(/\s\s+/g,'') );
+    });
+    resto.precio = $(".colSide > .restoPrecios").text().replace(/\s\s+/g,'');
+    resto.caracteristicas = new Array;
+    $(".goLink").find("li:not(.disabled)").each(function(i,e){
+        resto.caracteristicas.push( $(e).text().replace(/\s\s+/g,'') );
+    });
 }
 
 function get_resto_homepage() {
-	resto.homepage = $("iframe").attr("src");
+    resto.homepage = $("iframe").attr("src");
 }
 
 function next() {
-	var the_page = pages.pop();
-	if (!the_page) {
-		console.log("resto -> " + JSON.stringify(infor,undefined,3));
-		phantom.exit();
-	}
-	page.open(the_page.url, function(status) {
-	    if ( status === "success" ) {
-		page.evaluate(function() {
-		    the_page.extractor();	    
-        	});
-    		next();
-   	    } else {
-		console.log("could not load " + e + " st " + status);
-    	    }
-	});
+    var the_page = pages.pop();
+    console.log("//go for page " + the_page.url);
+    if (!the_page) {
+        console.log("resto = " + JSON.stringify(resto,undefined,3));
+        phantom.exit();
+    }
+    client.open(the_page.url, function(status) {
+        if ( status === "success" ) {
+            client.evaluate(function() {
+                the_page.extractor();	    
+            });
+            next();
+        } else {
+            console.log("could not load " + e + " st " + status);
+        }
+    });
 }
 
