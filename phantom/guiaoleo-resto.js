59e6b3b093e2a32782ac62f652fd8312486d1e78
Sebastian Javier Marchano <sebasjm@gmail.com>
2012-10-16 15:01:09 +0000

    fix

--- phantom/guiaoleo-resto.js
+++ phantom/guiaoleo-resto.js
@@ -4,22 +4,17 @@ var client = require('webpage').create();
 
 client.injectJs('lib/jquery.min.js');
 
-/*
-page.onConsoleMessage = function(msg) {
+client.onConsoleMessage = function(msg) {
     console.log(msg);
 };
+/*
 */
 var pages = new Array;
 var resto = new Object;
+var handler = new Object;
 
-pages.push({
-    url:system.args[1],
-    extractor: get_resto_data
-});
-next();
-
-
-function get_resto_data() {
+handler.get_resto_data = function (resto,pages,handler) {
+    console.log("getting resto data" + resto);
     var restoDatos = $(".restoDatos");
     resto.nombre = restoDatos.find(".restoDatosNombre").text().replace(/\s+/g, ' ');
     resto.direccion = restoDatos.find(".restoDatosDireccion").text().replace(/\s+/g, ' ');
@@ -37,8 +32,9 @@ function get_resto_data() {
         if ( !!href.match('restaurante/urlredirect/nombreClave') ) { 
             resto.site.push(href);
             pages.push({
-                url:'http://www.guiaoleo.com.ar/'+href, 
-                extractor: get_resto_homepage
+                url:'http://www.guiaoleo.com.ar/'+href,
+                extractor: handler.get_resto_homepage
+//
             });
         }
     });
@@ -74,28 +70,50 @@ function get_resto_data() {
     $(".goLink").find("li:not(.disabled)").each(function(i,e){
         resto.caracteristicas.push( $(e).text().replace(/\s\s+/g,'') );
     });
+    return resto;
 }
 
-function get_resto_homepage() {
-    resto.homepage = $("iframe").attr("src");
+handler.get_site = function(resto){
+	for (var i in resto.site) {
+            pages.push({
+                url:'http://www.guiaoleo.com.ar/'+resto.site[i],
+                extractor: handler.get_resto_homepage
+            });
+	}
 }
+
+handler.get_resto_homepage = function(resto) {
+    resto.homepage = $("iframe").attr("src");
+    return resto;
+}
+
+the_page = "";
 
 function next() {
-    var the_page = pages.pop();
-    console.log("//go for page " + the_page.url);
+    console.log("pages -> " + JSON.stringify(pages));
+    the_page = pages.pop();
     if (!the_page) {
         console.log("resto = " + JSON.stringify(resto,undefined,3));
-        phantom.exit();
+        phantom.exit(); return;
     }
+    console.log("//go for page " + JSON.stringify(the_page,undefined,3));
     client.open(the_page.url, function(status) {
         if ( status === "success" ) {
-            client.evaluate(function() {
-                the_page.extractor();	    
-            });
+            resto = client.evaluate(the_page.extractor,resto);
+	    if (!!the_page.done) the_page.done(resto);
             next();
         } else {
             console.log("could not load " + e + " st " + status);
         }
     });
 }
+
+pages.push({
+    url:system.args[1],
+    extractor: handler.get_resto_data
+    done: handler.get_site
+});
+
+next();
+
 
