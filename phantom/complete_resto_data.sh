2f53c339bc80a44eb80eb4fca8f9f225abf96221
Sebastian Javier Marchano <sebasjm@gmail.com>
2012-10-19 11:13:31 +0000

    more restos automatic

--- phantom/complete_resto_data.sh
+++ phantom/complete_resto_data.sh
@@ -1,6 +1,6 @@
-for line in $(tail -n+4520 restos.txt); do 
+for line in $(tail -n+5140 restos.txt); do 
 	echo resto $line
 	name=$(echo $line | sed 's/^.*-\([0-9]*\)$/\1/'); 
 	phantomjs --load-images=no guiaoleo-resto.js $line > resto-id-$name; 
-	sleep 5;
+	sleep 7;
 done
